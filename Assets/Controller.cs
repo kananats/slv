20685266084f9310118601aa0a94dd4b2f37146d
kris157 <kananat.s@gmail.com>
2015-08-09 00:39:06 +0700

    1st commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+    public static Controller instance;
+
+    public int[] accumulatedRounds;
+
+    public Form current;
+    public Form target;
+    public InputField result;
+
+    void Start()
+    {
+        instance = this;
+
+        current.level = 1;
+        current.percentage = 0;
+
+        target.level = 10;
+        target.percentage = 0;
+    }
+
+    public void Calculate()
+    {
+        int currentRound = SkillLevelToRound(current.level, current.percentage);
+        int targetRound = SkillLevelToRound(target.level, target.percentage);
+
+        int diff = targetRound - currentRound;
+
+        if (diff < 0)
+            diff = 0;
+
+        result.text = diff.ToString();
+    }
+
+    private int SkillLevelToRound(int level, int percentage)
+    {
+        return (int)(accumulatedRounds[level - 1] + percentage * 0.01f * (accumulatedRounds[level] - accumulatedRounds[level - 1]));
+    }
+}
---
+++ Assets/Form.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Form : MonoBehaviour
+{
+    public InputField levelField;
+    public InputField percentageField;
+
+    private int _level;
+    private int _percentage;
+
+    public int level
+    {
+        get
+        {
+            return _level;
+        }
+
+        set
+        {
+            _level = value;
+
+            if (_level >= 15)
+            {
+                _level = 15;
+                _percentage = 0;
+            }
+            else if (level < 1)
+            {
+                _level = 1;
+                _percentage = 0;
+            }
+
+            levelField.text = level.ToString();
+            percentageField.text = percentage.ToString();
+
+        }
+    }
+
+    public int percentage
+    {
+        get
+        {
+            return _percentage;
+        }
+
+        set
+        {
+            _percentage = value;
+
+            if (_percentage > 99)
+            {
+                _percentage = 99;
+            }
+            else if (_percentage < 0)
+            {
+                _percentage = 0;
+            }
+
+            levelField.text = level.ToString();
+            percentageField.text = percentage.ToString();
+        }
+    }
+
+    public void EV_EndEdit()
+    {
+        try
+        {
+            int level = int.Parse(levelField.text);
+            int percentage = int.Parse(percentageField.text);
+
+            this.level = level;
+            this.percentage = percentage;
+
+            Controller.instance.Calculate();
+        }
+
+        catch (System.Exception)
+        {
+            levelField.text = level.ToString();
+            percentageField.text = percentage.ToString();
+        }
+    }
+}
